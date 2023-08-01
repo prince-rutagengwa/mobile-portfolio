103610b3e3df1f8d23e3983ba7308f4a8fe74477
prince-rutagengwa <princerutagengwa20@gmail.com>
2023-08-01 10:30:30 +0200

    Add a navigation menu for mobile version

---
+++ index.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const menuContainer = document.querySelector('.menu-container');
+const timeBar = document.querySelector('.time-bar');
+const homeIcon = document.querySelector('.home-icon');
+const navMailIcon = document.querySelector('.nav-mail-icon');
+const navMailItem = document.querySelector('.nav-mail-item');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  menuContainer.classList.toggle('active');
+  timeBar.classList.toggle('active');
+  homeIcon.classList.toggle('active');
+  navMailIcon.classList.toggle('active');
+  navMailItem.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => {
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    menuContainer.classList.remove('active');
+    timeBar.classList.remove('active');
+  });
+});
